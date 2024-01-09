bcea057523f7434906a7754b2435fff99de38917
samirbenbouker2 <sbb.cmpany@gmail.com>
2024-01-09 13:11:20 +0100

    setup project with dependencies and properties configuration

---
+++ src/main/java/com/bcnc/inditex/BcncInditexApplication.java
@@ -0,0 +1,13 @@
+package com.bcnc.inditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcncInditexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcncInditexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bcnc/inditex/BcncInditexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcnc.inditex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcncInditexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

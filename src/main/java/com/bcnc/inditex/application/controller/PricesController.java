b2576dc864580372c3928a7db6d30f313f874854
samirbenbouker2 <sbb.cmpany@gmail.com>
2024-01-10 12:20:02 +0100

    add controller for expose endpoints

---
+++ src/main/java/com/bcnc/inditex/application/controller/PricesController.java
@@ -0,0 +1,30 @@
+package com.bcnc.inditex.application.controller;
+
+import com.bcnc.inditex.application.DTO.PricesDTO;
+import com.bcnc.inditex.application.request.PricesRequestDTO;
+import com.bcnc.inditex.application.service.PricesService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PricesController {
+
+    @Autowired
+    private PricesService pricesService;
+
+    @GetMapping
+    public List<PricesDTO> getAll() {
+        return pricesService.getAll();
+    }
+
+    @GetMapping("/filter")
+    public PricesDTO getPricesByStartDateProductIdBrandId(@RequestBody @Valid PricesRequestDTO pricesRequestDTO) {
+        return pricesService.getPricesByStartDateProductIdBrandId(pricesRequestDTO);
+    }
+
+}
